36a6e8d4af2b93131035c47f00554e1741319dad
itc-s26004 <s26004@std.it-college.ac.jp>
2026-07-15 02:46:57 +0000

    4-3

---
+++ chap04/p157.py
@@ -0,0 +1,5 @@
+def show_args(*args):
+    print('Positional arguments:', args)
+    return args
+
+print(show_args(1,2,3,'da!'))
---
+++ chap04/p157_2.py
@@ -0,0 +1,5 @@
+def show_Kwargs(**Kwargs):
+    print('Keyword arguments:', Kwargs)
+    return Kwargs
+
+print(show_Kwargs(pasta = 'ペンネ',drink = '赤ワイン',main_dish = '肉料理',n_customers=3))
