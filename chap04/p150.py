e8ec965720022ed941627b2d25e2074fcb37981c
itc-s26004 <s26004@std.it-college.ac.jp>
2026-07-15 01:44:04 +0000

    4-2

---
+++ chap04/p150.py
@@ -0,0 +1,3 @@
+def house_for_rent(bedrooms, walk_min,house_type,rent_yen):
+    return {'bedrooms':bedrooms, 'walk_min': walk_min,'house_type': house_type,'rent_yen':rent_yen}
+print(house_for_rent(2,15,'アパート',50))
