3df54783840b3f770b1639f1515e87079ed309e6
itc-s26004 <s26004@std.it-college.ac.jp>
2026-08-04 01:15:16 +0000

    6-1

---
+++ chap06/q6_1_2.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self,
+                 name = '',
+                 nationality = '',
+                 birth = '',
+                 address = ''):
+        self.name = name
+        self.nationality = nationality
+        self.birth = birth
+        self.address = address
+
+    def show_attributes(self):
+        print("名前:", self.name)
+        print("国籍:", self.nationality)
+        print("生まれたとし:", self.birth)
+        print("住んでいる所:", self.address)
+       
+heroine = Person('かぐや姫','日本','685','静岡県富士市')
+(heroine.show_attributes())
+
+
+hero = Person('金太郎','日本','956','静岡県駿東都小山町')
+(hero.show_attributes())
+
