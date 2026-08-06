a4ff4b72f8ba70107ec08caf629ba5501cae3055
itc-s26004 <s26004@std.it-college.ac.jp>
2026-08-06 03:45:04 +0000

    6-3

---
+++ chap06/q6_3_1.py
@@ -0,0 +1,19 @@
+class Nigiri:
+    category = "にぎり"
+    top = "ねた"
+    base = "しゃり"
+
+    def show_attributes(self):
+        print("top: {}, base: {}, category: {}".format(self.top, self.base, self.category))
+        print("price: {}円".format(self.price))
+class Katsuo(Nigiri):
+    top = "かつお"
+    topping = "生姜とねぎ"
+    price = 100
+
+    def show_attributes(self):
+        super().show_attributes()
+        print("topping: {}".format(self.topping))
+
+k1 = Katsuo()
+k1.show_attributes()
